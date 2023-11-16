2e60fddbd1d6930e6d0891648519eaeb467eb76c
braniubojni <erik95harutyunyan@gmail.com>
2023-11-16 23:32:01 +0400

    eh/feat-code-splitting-lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
 import Counter from './components/Counter'
 import './index.scss'
+import { Link } from 'react-router-dom'
+import LazyLoad from './components/common/LazyLoad'
+import { lazy } from 'react'
+
+const AboutPage = lazy(() => import('./Pages/AboutPage/AboutPage'));
+const MainPage = lazy(() => import('./Pages/MainPage/MainPage'));
+
 
 const App = () => {
 	return (
 		<div className='app'>
-			asdkjhkhkjhkjhjk
+			<Link to="/">Main</Link>
+			<Link to="/about">About</Link>
+			<Routes>
+				<Route path='/about' element={
+					<LazyLoad>
+						<AboutPage />
+					</LazyLoad>
+				} />
+				<Route path='/' element={
+					<LazyLoad>
+						<MainPage />
+					</LazyLoad>} />
+			</Routes>
 			<Counter />
 		</div>
 	)
