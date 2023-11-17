a5d265fd2407ea5fe82c31552758b996f508ba9e
braniubojni <erik95harutyunyan@gmail.com>
2023-11-17 10:51:35 +0400

    eh/feat-themes-implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
+import { lazy, useContext } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 import Counter from './components/Counter'
-import './index.scss'
-import { Link } from 'react-router-dom'
 import LazyLoad from './components/common/LazyLoad'
-import { lazy } from 'react'
+import './styles/index.scss'
+import { Theme, ThemeContext } from './theme/ThemeContext'
+import { useTheme } from './theme/useTheme'
 
 const AboutPage = lazy(() => import('./Pages/AboutPage/AboutPage'));
 const MainPage = lazy(() => import('./Pages/MainPage/MainPage'));
 
 
 const App = () => {
+	const { theme, toggleTheme } = useTheme();
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Toggle</button>
 			<Link to="/">Main</Link>
 			<Link to="/about">About</Link>
 			<Routes>
